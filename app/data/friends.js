1ed172a6c4075cdf8a43fd538560596f2c1ca7fd
Kent Valle <kentvalle1@gmail.com>
2020-05-14 04:29:14 -0500

    it doesnt exactly work, but its something

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+        name:"Doom Guy",
+        photo: "https://images.app.goo.gl/C2Ca6vXkRzM4v6TY8",
+        scores: [
+            "5",
+            "3",
+            "1",
+            "4",
+            "2",
+            "3",
+            "5",
+            "5",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name:"Animal Crossing Villager",
+        photo: "https://images.app.goo.gl/wkiF7ciXSnc5jVVD8",
+        scores: [
+            "1",
+            "5",
+            "5",
+            "2",
+            "4",
+            "2",
+            "1",
+            "4",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name:"Steve",
+        photo: "https://images.app.goo.gl/wkiF7ciXSnc5jVVD8",
+        scores: [
+            "2",
+            "5",
+            "2",
+            "3",
+            "3",
+            "2",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    }
+
+];
+
+module.exports = friends;
